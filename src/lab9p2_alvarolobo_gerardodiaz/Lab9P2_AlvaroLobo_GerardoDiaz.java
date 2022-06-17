92f465207be4a92506b4cda698eb37e905da34da
alvarolobooooooooooooooooooo <105078434+alvarolobooooooooooooooooooo@users.noreply.github.com>
2022-06-17 13:39:58 -0600

    primer comentario

---
+++ src/lab9p2_alvarolobo_gerardodiaz/Lab9P2_AlvaroLobo_GerardoDiaz.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab9p2_alvarolobo_gerardodiaz;
+
+/**
+ *
+ * @author agln7
+ */
+public class Lab9P2_AlvaroLobo_GerardoDiaz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
